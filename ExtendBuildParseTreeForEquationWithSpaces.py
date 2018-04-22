19f65755e5b473633e433b4521f6002c2008d45d
Derek Roman <derek.roman81@gmail.com>
2018-04-22 16:09:19 -0500

    Completeing the Week 6 Assignment

---
+++ ExtendBuildParseTreeForEquationWithSpaces.py
@@ -0,0 +1,48 @@
+#F. Derek Roman - GitHub Profile CodeNamor
+#SWDV-610-3W Data Structures Unit 6
+
+#define imports needed for this module
+from Stack import *
+from BinaryTree import BinaryTree
+
+#define the buildParseTree function
+def buildParseTree(treeExpression):
+    list = treeExpression.split()
+    pStack = Stack
+    bTree = BinaryTree
+    pStack.push(list)
+    currentTree = bTree
+    #follwoing the rules for buiding a parse tree
+    for i in list:
+        #if '( ' is the first token, by Rule 1 of parse trees add a new left node
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRootVal(int(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            #if there is a problem raise a Value Error
+            raise ValueError
+    #return the tree
+    return bTree
+
+def main():
+    #if spaces are added to the equation
+    mathEq = "(  (  10  +  5  )  *  3  ) "
+    #remove the extra spaces
+    mathEqStrip = mathEq.strip(" ")
+    #send the stripped equation to the build Parse Tree function
+    pt = buildParseTree(mathEqStrip)
+    pt.postorder()
+    
+main()
---
+++ Stack.py
@@ -0,0 +1,14 @@
+#class for implementing stacks
+class Stack():
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def push(self):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[len(self.items)-1]
+    def size(self):
+        return len(self.items)
